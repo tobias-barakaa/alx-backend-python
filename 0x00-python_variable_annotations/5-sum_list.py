532bd81e39b37e118c5b4968af107537894dcfbd
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-07 12:59:10 -0800

    adding more fixture

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 from typing import List
 
-"""Define and annotate the following variables with the specified values."""
+"""define and annotate the following variables with the specified values:"""
 
 
 def sum_list(input_list: List[float]) -> float:
     """
-    Takes a list of floats (input_list) as an argument
+    Takes a list of floats (input_list) as an
+    argument
     and returns their sum as a float.
     """
+    if not all(isinstance(x, float) for x in input_list):
+        raise TypeError("All elements in the list must be floats.")
     return sum(input_list)
