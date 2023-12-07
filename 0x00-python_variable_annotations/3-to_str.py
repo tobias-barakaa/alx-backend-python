ea3b5ffa86190b8c36dc4b0e52e7f0cad1433bff
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-07 11:32:07 -0800

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Basic annotations - floor"""
+
+
+def floor(n: float) -> str:
+    """Takes a float n as argument and returns the string rep of float."""
+    return str(n)
