49b1269a6132372ac4449dabb1997166b2175226
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-07 10:33:09 -0800

    adding files

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""funtion that adds two float number"""
+
+
 def add(a: float, b: float) -> float:
     """Add two floats and return the result as a float"""
     return a + b
