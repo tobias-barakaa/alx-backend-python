edc487ae2849ff0eea4c3ad8683a415b9623e540
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-07 11:30:08 -0800

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""function to return floor number"""
+
+
+def floor(n: float) -> int:
+    """Takes a float n as argument and returns the floor of the float."""
+    return int(n)
