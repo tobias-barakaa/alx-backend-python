34afd43e1d12ecef1b2cea9c2312521917e2b5b8
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-07 11:37:56 -0800

    4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""define and annotate the following variables with the specified values:"""
+
+
+a:int = 1
+pi:float = 3.14
+i_understand_annotations:bool = True
+school:str = "Holberton"
