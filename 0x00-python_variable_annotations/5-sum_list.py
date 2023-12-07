e4fdc2fce088ddcf54d84150489113fd4817cba5
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-07 13:40:04 -0800

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 from typing import List
 
-"""define and annotate the following variables with the specified values:"""
+"""Define and annotate the following variables with the specified values."""
 
 
 def sum_list(input_list: List[float]) -> float:
     """
-    Takes a list of floats (input_list) as 
-    an argument
+    Takes a list of floats (input_list) as an argument
     and returns their sum as a float.
     """
-    if not input_list:
-        return 0.0
     return sum(input_list)
