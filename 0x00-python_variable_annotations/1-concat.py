88ccb03f0691be78cd2d6fd25306931ca9924dfc
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-07 10:51:20 -0800

    concatenate-string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""function to concatenate string"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings 
+    with a space in between
+    """
+    return str1 + " " + str2
