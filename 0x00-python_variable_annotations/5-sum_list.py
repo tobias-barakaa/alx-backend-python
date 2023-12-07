266d9cbb083a192b37966d4bbf0be041c8be7545
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-07 11:59:43 -0800

    adding more arguments

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,5 +5,8 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """Takes a list input_list of floats as argument and returns their sum as a float."""
+    """
+    Takes a list input_list of floats as argument and
+    returns their sum as a float.
+    """
     return sum(input_list)
