37cd812388ce291885288337837b78e113dde305
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-07 10:56:09 -0800

    1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
-"""function to concatenate string"""
+"""Function to concatenate two strings with a space in between."""
 
 def concat(str1: str, str2: str) -> str:
     """
-    Concatenate two strings 
-    with a space in between
+    Concatenate two strings with a space in between.
+
+    Parameters:
+    str1 (str): The first string.
+    str2 (str): The second string.
+
+    Returns:
+    str: The concatenated string.
     """
-    return str1 + " " + str2
+    return f"{str1} {str2}"
