fa855bdfbdf64cba74a8626a0d076a141b61273f
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-07 14:21:38 -0800

    function to return tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to_kv
+"""
+from typing import List, Union,Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Takes a string k and an int OR float v as
+    arguments and returns a tuple.
+    """
+    return (k, v**2)
