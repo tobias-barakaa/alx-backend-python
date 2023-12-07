62e48b03ee98df5abf70982fe3c5b06335dad3f3
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-07 11:47:19 -0800

    fixing pep8

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,5 +4,8 @@
 
 
 def sum_list(input_list: float) -> float:
-    """Takes a list input_list of floats as argument and returns their sum as a float."""
+    """
+    Takes a list input_list of floats as argument and
+    returns their sum as a float.
+    """
     return sum(input_list)
