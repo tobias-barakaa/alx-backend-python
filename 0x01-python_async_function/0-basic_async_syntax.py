09e94f6fe74a8e9d3890def2c570f322afab1466
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-11 09:36:34 -0800

    finished task

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,10 +5,11 @@ Python function to print
 import asyncio
 import random
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay:int = 10) -> float:
     """
     funtion that return
     random value(float)
     """
-    task = asyncio.sleep(random.uniform(0, max_delay))
-    return await task
+    task = random.uniform(0,max_delay)
+    await asyncio.sleep(task)
+    return task
