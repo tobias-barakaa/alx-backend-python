d976772934a854d4afee091e6c84b2cfab9cd7dc
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-11 12:17:50 -0800

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,6 +3,7 @@
 import function and use it
 """
 import asyncio
+from typing import List
 from 0-basic_async_syntax import wait_random
 """
 function that return
@@ -10,7 +11,7 @@ list of functions
 """
 
 
-async def max_delay(n: int, max_delay: int) -> list:
+async def max_delay(n: int, max_delay: int) -> List[float]:
     delays = [await wait_random(max_delay) for _ in range(n)]
     delays.sort()
     return delays
