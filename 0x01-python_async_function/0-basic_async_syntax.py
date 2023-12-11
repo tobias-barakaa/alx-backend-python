461b5194f5fbb402e9b241f2b434b3f8ddaed920
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-11 10:20:26 -0800

    0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay:int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     funtion that return
     random value(float)
