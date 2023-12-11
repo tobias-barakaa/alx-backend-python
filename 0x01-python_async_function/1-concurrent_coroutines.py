e584d2c063faa1007c4fd01bc91410f744c6a4e8
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-11 14:06:45 -0800

    adding changes

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 """
-import function and use it
+Import wait_random from the previous python file and write an async routine
 """
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
-"""
-function that return
-list of functions
-"""
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def max_delay(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Async routine that spawns wait_random n times with the specified max_delay.
+
+    Args:
+        n (int): Number of times to spawn wait_random.
+        max_delay (int): The maximum delay value.
+
+    Returns:
+        List[float]: List of delays (float values) in ascending order.
+    """
     delays = [await wait_random(max_delay) for _ in range(n)]
     delays.sort()
     return delays
