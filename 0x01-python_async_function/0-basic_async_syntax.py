78a33a4f88f19ad69e880d4a42e6edf12935faed
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-11 09:44:04 -0800

    pycodestyle fixing

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,11 +5,12 @@ Python function to print
 import asyncio
 import random
 
-async def wait_random(max_delay:int = 10) -> float:
+
+async def wait_random(max_delay:int = 10) -> float :
     """
     funtion that return
     random value(float)
     """
-    task = random.uniform(0,max_delay)
+    task = random.uniform(0, max_delay)
     await asyncio.sleep(task)
     return task
