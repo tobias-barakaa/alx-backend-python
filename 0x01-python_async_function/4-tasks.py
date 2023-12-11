9c8308d5a2c23051a56ab039ff430f46369e2d66
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-11 15:09:10 -0800

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,8 +7,11 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Regular function syntax that returns an asyncio.Task
+    Coroutine to spawn task_wait_random n times with the specified max_delay
+    Returns a list of delays in ascending order
     """
-    return asyncio.create_task(wait_random(max_delay))
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    return sorted(results)
