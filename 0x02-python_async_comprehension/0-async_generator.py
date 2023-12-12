478f70f57d1b01d3584c43f13594e9ed97c34dfa
tobias-barakaa <ngairato@gmail.com>
2023-12-12 05:03:32 -0800

    0-async_generator.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+coroutine function that take no argument
+"""
+import random
+import asyncio
+
+
+async def async_generator():
+    """
+    coroutine that yields random number
+    between 1 to 10
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
