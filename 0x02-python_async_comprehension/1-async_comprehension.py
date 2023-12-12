31fd2b428c63da7dab7cdbb892cf65aae18b9925
tobias-barakaa <ngairato@gmail.com>
2023-12-12 13:30:00 -0800

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,8 +4,8 @@
 coroutine function that take no argument
 """
 import random
-async_generator = __import__('0-async_generator').async_generator
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
