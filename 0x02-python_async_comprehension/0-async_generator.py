82d0753d95dbceeb3f9cefcc3a48bce626a30123
tobias-barakaa <ngairato@gmail.com>
2023-12-12 10:22:48 -0800

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,13 +7,11 @@ import random
 
 async def async_generator() -> float:
     """
-    Asynchronous generator that yields random numbers
-    between 0 and 10 after waiting for 1 second.
+    Asynchronous generator that yields random numbers between 0 and 10 after waiting for 1 second.
 
     Yields:
         float: A random float value between 0 and 10.
     """
-    while count < 10:
+    for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
-        count += 1
+        yield random.randint(0, 10)
