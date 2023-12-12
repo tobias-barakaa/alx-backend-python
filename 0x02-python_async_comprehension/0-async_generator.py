f8a28a6a6492916fb716ff8b2ca61f49e280b435
tobias-barakaa <ngairato@gmail.com>
2023-12-12 07:35:56 -0800

    no matter what I must win

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 """
-coroutine function that takes no argument
+coroutine function that takes no arguments
 """
 import asyncio
 import random
 
-
 async def async_generator() -> float:
     """
-    coroutine that yields random numbers
-    between 1 to 10
+    Coroutine that yields random numbers between 0 and 10 after waiting for 1 second.
+
+    Returns:
+        float: A random float value between 0 and 10.
     """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
+
