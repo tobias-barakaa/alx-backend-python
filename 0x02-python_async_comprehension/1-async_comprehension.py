a51bab6ba2d50987b69dd1fdfaed756b6314df1d
tobias-barakaa <ngairato@gmail.com>
2023-12-12 13:26:33 -0800

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,11 +5,12 @@ coroutine function that take no argument
 """
 import random
 async_generator = __import__('0-async_generator').async_generator
+from typing import List
 
 
-async def async_comprehension() -> float:
+async def async_comprehension() -> List[float]:
     """
     coroutine that collect 10 random numbers
     between 0 and 10
     """
-    return [random.uniform(0, 10) for _ in range(10)]
+    return [i async for i in async_generator()]
