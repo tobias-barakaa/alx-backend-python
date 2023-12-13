53d10fac3beebc7820ed9ccf4ba986e91396cedf
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-13 11:44:47 -0800

    2-masure-runtim.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 
 """
-coroutine function that take no argument
+coroutine function that takes no argument
 """
 import asyncio
-from typing import List
 import time
 from random import uniform
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     Measures the runtime of a coroutine.
 
     Returns:
         float: The total runtime in seconds.
     """
-    start_time = time.perf_counter()
+    start_time = time.time()
 
     await asyncio.gather(async_comprehension() for _ in range(4))
-
-    end_time = time.perf_counter()
+    end_time = time.time()
     return end_time - start_time
+
