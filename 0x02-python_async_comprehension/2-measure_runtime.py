ca3a1216d4f0cec065b0af871368e88f0de25bbb
tobias-barakaa <ngairato@gmail.com>
2023-12-13 04:40:59 -0800

    fixing pycodestyle

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,6 +9,7 @@ import time
 from random import uniform
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """
     Measures the total runtime and return it.
