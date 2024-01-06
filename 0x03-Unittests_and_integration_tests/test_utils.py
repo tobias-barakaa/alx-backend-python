e9d0751aca062a2a12adbdd34c77e070cfca317d
tobias-barakaa <tobiasbarakan@gmail.com>
2024-01-06 14:57:53 -0800

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -35,19 +35,15 @@ class TestAccessNestedMap(unittest.TestCase):
 
 class TestGetJson(unittest.TestCase):
     
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
     @patch('requests.get')
-    def test_get_json(self, mock_get):
-        test_data = [
-            {"test_url": "http://example.com", "test_payload": {"payload": True}},
-            {"test_url": "http://holberton.io", "test_payload": {"payload": False}}
-        ]
-        
-        for data in test_data:
-            test_url = data["test_url"]
-            test_payload = data["test_payload"]
-            mock_response = Mock()
-            mock_response.json.return_value = test_payload
-            mock_get.return_value = mock_response
-            result = get_json(test_url)
-            mock_get.assert_called_once_with(test_url)
-            self.assertEqual(result, test_payload)
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
