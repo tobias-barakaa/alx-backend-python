526501c72aa27b600477af07aaf710f930dbe2b1
tobias-barakaa <tobiasbarakan@gmail.com>
2024-01-06 12:53:13 -0800

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ function test
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -20,14 +21,10 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
     @parameterized.expand([
-        ({}, ("a",), "Key 'a' not found in nested map"),
-        ({"a": 1}, ("a", "b"), "Key 'b' not found in nested map"),
+       ({}, ("a",), "a"),
+       ({"a": 1}, ("a", "b"), "b"),
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expe_exep):
-        """
-        using assertRaises
-        """
+    def test_access_nested_map_exception(self, nested_map, path, expected_key):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
-
-        self.assertEqual(str(context.exception), expe_exep)
+        self.assertEqual(context.exception.args[0], expected_key)
