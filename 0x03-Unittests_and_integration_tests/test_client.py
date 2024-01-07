40ee566befcdfee06f489a3976c2df19fd49684f
tobias-barakaa <ngairato@gmail.com>
2024-01-07 07:29:41 -0800

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -16,11 +16,14 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch("client.get_json")
     def test_org(self, org_name, mock_get_json):
-        """Tests that org() returns the correct value and calls get_json once.
+        """
+        Tests that org() returns the correct value and calls
+        get_json once.
         """
         mock_get_json.return_value = {"key": "value"}
         client = GithubOrgClient(org_name)
         org_data = client.org()
         self.assertEqual(org_data, {"key": "value"})
-        mock_get_json.assert_called_once_with
-        (client.ORG_URL.format(org=org_name))
+        mock_get_json.assert_called_once_with(
+            client.ORG_URL.format(org=org_name)
+        )
