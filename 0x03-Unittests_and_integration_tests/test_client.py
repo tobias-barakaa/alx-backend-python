afbeadf35569a04cb2cb4536fb00cba0244b4086
tobias-barakaa <ngairato@gmail.com>
2024-01-07 08:40:55 -0800

    changed many things

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-"""Test suite for the GithubOrgClient class."""
+"""
+Test suite for the GithubOrgClient class.
+"""
 
 import unittest
 from unittest.mock import patch
@@ -9,21 +11,17 @@ from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-
+    """TestGithubOrgClient class."""
     @parameterized.expand([
-        ("google", ),
-        ("abc", )
-    ])
-    @patch("client.get_json")
+            ("google"),
+            ("abc"),
+        ])
+    @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
         """
-        Tests that org() returns the correct value and calls
-        get_json once.
+        Test that GithubOrgClient.org returns the correct value.
         """
-        mock_get_json.return_value = {"key": "value"}
         client = GithubOrgClient(org_name)
-        org_data = client.org()
-        self.assertEqual(org_data, {"key": "value"})
+        client.org()
         mock_get_json.assert_called_once_with(
-            client.ORG_URL.format(org=org_name)
-        )
+            f"https://api.github.com/orgs/{org_name}")
