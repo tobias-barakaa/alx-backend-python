0ed577864ad2436d15e11159ce1c71e27aaec117
tobias-barakaa <ngairato@gmail.com>
2024-01-07 06:42:26 -0800

    pushing more

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,37 +2,24 @@
 
 """Test suite for the GithubOrgClient class."""
 
-from parameterized import parameterized
 import unittest
 from unittest.mock import patch
+from parameterized import parameterized
 from client import GithubOrgClient
-from typing import Dict
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """
-    TestGithubOrgClient class.
-    """
 
     @parameterized.expand([
         ("google",),
         ("abc",),
     ])
-    @patch('client.get_json', return_value={'example_key': 'example_value'})
+    @patch("client.get_json")
     def test_org(self, org_name, mock_get_json):
-        """
-        Test the GithubOrgClient.org method.
-
-        Parameters:
-            - org_name: Name of the GitHub organization to test.
-            - mock_get_json: Mock object for the get_json function.
-
-        This test verifies that the GithubOrgClient.org method returns the
-        expected result and that the get_json function is called with the
-        correct URL.
-        """
+        """Tests that org returns the correct value and calls get_json once."""
+        mock_get_json.return_value = {}
         client = GithubOrgClient(org_name)
-        result = client.org()
-        expected_url = f'https://api.github.com/orgs/{org_name}'
-        mock_get_json.assert_called_once_with(expected_url)
-        self.assertEqual(result, {'example_key': 'example_value'})
+        org_data = client.org()
+
+        mock_get_json.assert_called_once_with(client.ORG_URL.format(org=org_name))
+        self.assertEqual(org_data, {})
