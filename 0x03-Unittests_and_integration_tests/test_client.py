c6f2cabf91ffff42a3004e9f17113453201e8a88
tobias-barakaa <ngairato@gmail.com>
2024-01-07 06:38:32 -0800

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-"""Generic utilities for github org client.
-"""
+
+"""Test suite for the GithubOrgClient class."""
 
 from parameterized import parameterized
 import unittest
-from client import GithubOrgClient
 from unittest.mock import patch
+from client import GithubOrgClient
 from typing import Dict
 
 
 class TestGithubOrgClient(unittest.TestCase):
+    """
+    TestGithubOrgClient class.
+    """
 
     @parameterized.expand([
         ("google",),
@@ -17,6 +20,17 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch('client.get_json', return_value={'example_key': 'example_value'})
     def test_org(self, org_name, mock_get_json):
+        """
+        Test the GithubOrgClient.org method.
+
+        Parameters:
+            - org_name: Name of the GitHub organization to test.
+            - mock_get_json: Mock object for the get_json function.
+
+        This test verifies that the GithubOrgClient.org method returns the
+        expected result and that the get_json function is called with the
+        correct URL.
+        """
         client = GithubOrgClient(org_name)
         result = client.org()
         expected_url = f'https://api.github.com/orgs/{org_name}'
