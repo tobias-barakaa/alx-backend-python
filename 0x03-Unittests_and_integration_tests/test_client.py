5401a0bce8405a7739dec8b7d7b25a9aada10922
tobias-barakaa <ngairato@gmail.com>
2024-01-07 07:05:45 -0800

    now it should work

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -9,13 +9,19 @@ from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    @patch('client.get_json')
+
     @parameterized.expand([
         ("google",),
         ("abc",),
     ])
+    @patch("client.get_json")
     def test_org(self, org_name, mock_get_json):
+        """Tests that org() returns the correct value and calls get_json() once with the expected argument."""
+
+        expected_url = GithubOrgClient.ORG_URL.format(org=org_name)
+        expected_return_value = {"key": "value"}
+        mock_get_json.return_value = expected_return_value
         client = GithubOrgClient(org_name)
-        client.org()
-        mock_get_json.assert_called_once_with
-        (f"https://api.github.com/orgs/{org_name}")
+        result = client.org()
+        mock_get_json.assert_called_once_with(expected_url)
+        self.assertEqual(result, expected_return_value)
