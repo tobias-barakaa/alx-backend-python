2aefb71e16d0029560758ab8870cc945f966bfcd
tobias-barakaa <ngairato@gmail.com>
2024-01-07 14:41:58 -0800

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -57,3 +57,15 @@ class TestGithubOrgClient(unittest.TestCase):
                 self.assertEqual(result, ["Google", "Twttr"])
                 mock_public_repos_url.assert_called_once_with()
                 mock_get_json.assert_called_once_with(payload)
+
+    @parameterized.expand([
+            ({'license': {'key': 'my_license'}}, "my_license", True),
+            ({'license': {'key': 'other_license'}}, "my_license", False),
+        ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+        Test that GithubOrgClient.has_license returns the correct value.
+        """
+        client = GithubOrgClient("x")
+        result = client.has_license(repo, license_key)
+        self.assertEqual(result, expected)
