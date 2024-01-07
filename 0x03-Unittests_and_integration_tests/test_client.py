381c0e2b3ff391a5cee18e75dd1136b5c512a3fd
tobias-barakaa <ngairato@gmail.com>
2024-01-07 06:36:03 -0800

    pycodestyle

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -20,6 +20,5 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         result = client.org()
         expected_url = f'https://api.github.com/orgs/{org_name}'
-        
         mock_get_json.assert_called_once_with(expected_url)
         self.assertEqual(result, {'example_key': 'example_value'})
